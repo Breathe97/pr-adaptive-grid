d65e2dd999fb83948f09f711c58a5a67984a8ec4
Breathe <704340916@qq.com>
2026-05-27 17:31:00 +0800

    d

---
+++ demo/src/getLayout.ts
@@ -0,0 +1,13 @@
+export const getLayout = (ids: string[], mode: '1' | '2', ext: { fullId: string }) => {
+  switch (mode) {
+    case '1':
+      {
+      }
+      break
+    case '2':
+      {
+        const { fullId } = ext
+      }
+      break
+  }
+}
