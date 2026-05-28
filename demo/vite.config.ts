77cc6e9ac6a6309bc2a5b1f5f4a4be8cc5f6accb
Breathe <704340916@qq.com>
2026-05-28 16:45:21 +0800

    d

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+import { name } from '../package.json'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: `/${name}`,
+  server: {
+    // port: 1234,
+    host: '0.0.0.0'
+  },
+  build: {
+    outDir: `dist_${name}`
+  }
 })
